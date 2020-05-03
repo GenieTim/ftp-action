f232bb609ba8a6b718f78f5054dc4853ef714261
Tomasz Trębski <kornicameister@gmail.com>
2020-05-03 22:49:04 +0200

    Treat  symbolic links as files
    Ref: https://lftp.yar.ru/lftp-man.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ lftp ${INPUT_HOST} -u ${INPUT_USER},${INPUT_PASSWORD} -e "
   set ftp:ssl-force $INPUT_FORCESSL; 
   set sftp:auto-confirm yes;
   set ssl:verify-certificate $INPUT_FORCESSL; 
-  mirror -v -P 5 -R -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR;
+  mirror -v -P 5 -R -L -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR;
   quit
 "
