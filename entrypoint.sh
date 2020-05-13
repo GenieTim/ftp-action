d50202305c1f490c86459ae521b183de4ae82eca
Tomasz Trębski <kornicameister@gmail.com>
2020-05-14 00:47:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 
 lftp ${INPUT_HOST} -u ${INPUT_USER},${INPUT_PASSWORD} -e "
-  set net:timeout 60;
-  set net:max-retries 20;
+  set net:timeout 10;
+  set net:max-retries 1;
   set net:reconnect-interval-multiplier 1;
   set net:reconnect-interval-base 5;
   set ftp:ssl-force $INPUT_FORCESSL; 
   set sftp:auto-confirm yes;
   set ssl:verify-certificate $INPUT_FORCESSL; 
-  mirror -v -P 5 -R -L -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR;
+  mirror -v -P 5 -R -n -L -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR;
   quit
 "
