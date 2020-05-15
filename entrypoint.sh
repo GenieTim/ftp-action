efa995c20d2070fd1ab9d894d00dc375c5e5c6a0
Tomasz Trębski <kornicameister@gmail.com>
2020-05-16 01:24:50 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
 lftp ${INPUT_HOST} -u ${INPUT_USER},${INPUT_PASSWORD} -e "
-  set net:timeout 10;
-  set net:max-retries 1;
-  set net:reconnect-interval-multiplier 1;
+  set net:timeout 60;
+  set net:max-retries 20;
+  set net:reconnect-interval-multiplier 2;
   set net:reconnect-interval-base 5;
   set ftp:ssl-force $INPUT_FORCESSL; 
   set sftp:auto-confirm yes;
